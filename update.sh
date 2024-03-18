041a2b07c9a813eb1a2efda27c5e0bbddc7c320f
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-03-18 16:41:55 -0500

    Update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # stop the running containers
-# docker-compose -p my-ftp -f ./ftp.yml down
+docker-compose -p my-nginx -f ./nginx.yml down
 
 # pull the latest changes from the remote repository
 git get
