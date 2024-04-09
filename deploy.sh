8bd7956c7523b935ae00725665bf6b1770a3fbf4
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-04-09 16:23:56 -0500

    Update

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# stop the running stack nginx
+docker stack rm nginx
+
+# pull the latest changes from the remote repository
+git get
+
+# build the new image
+docker build --no-cache -t my-nginx -f ./Dockerfile .
+
+# deploy the stack nginx
+docker stack deploy -c ./nginx.yml nginx
+
+# list the running containers
+docker ps -a
--- down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# stop the running containers
-docker-compose -p my-nginx -f ./nginx.yml down
-
-# list the running containers
-docker ps -a
